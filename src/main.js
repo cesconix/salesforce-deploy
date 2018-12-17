6d95666b18f568fddb556ea6b6b2e219f21c61c3
Francesco Pasqua <cesconix@me.com>
2018-12-17 19:04:44 +0100

    refactor: clean main file

--- src/main.js
+++ src/main.js
@@ -1,17 +1,1 @@
-import authenticate from './helpers/authenticate'
-
-const main = async () => {
-  try {
-    const token = await authenticate({
-      clientId: '',
-      clientPassword: ''
-    })
-    console.log(token)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-main()
-
-export default main
+export default {}
