375cecd582ea4e766fec3bd52fd4aaaccce2a799
Francesco Pasqua <cesconix@me.com>
2019-01-10 15:38:35 +0100

    chore: release v1.0.0

--- src/main.js
+++ src/main.js
@@ -1,1 +1,49 @@
-export default {}
+import fs from 'fs'
+import axios from 'axios'
+import find from './helpers/find'
+import mkdir from './helpers/mkdir'
+import zip from './helpers/zip'
+import upload from './helpers/upload'
+import unzip from './helpers/unzip'
+import rm from './helpers/rm'
+
+const deploy = async ({ hostname, username, password, cartridges, codeVersion }) => {
+  if (typeof cartridges === 'string') {
+    cartridges = [ { source: cartridges } ]
+  }
+
+  try {
+    cartridges.map(c => c.source).forEach(fs.accessSync)
+  } catch (e) {
+    throw new Error('Cartridges property contains invalid folders')
+  }
+
+  const instance = axios.create({
+    baseURL: `https://${hostname}/on/demandware.servlet/webdav/Sites`,
+    auth: { username, password }
+  })
+
+  const dest = `/Cartridges/${codeVersion}`
+
+  try {
+    const codeVersionExists = await find(dest, instance)
+
+    if (codeVersionExists) {
+      throw new Error('Code version exists')
+    }
+
+    await mkdir(dest, instance)
+
+    let file
+
+    file = await zip(cartridges, process.env.TMPDIR, codeVersion)
+    file = await upload(file, dest, instance)
+
+    await unzip(file, instance)
+    await rm(file, instance)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+export default deploy
