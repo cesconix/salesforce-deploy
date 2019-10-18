b7c91ab11659a318f222699b506a9406129b6d67
Francesco Pasqua <cesconix@me.com>
2019-10-18 11:49:51 +0200

    fix: change webdav method unzip to UNZIP

--- test/unzip.spec.js
+++ test/unzip.spec.js
@@ -7,19 +7,25 @@ const file = '/Cartridges/v1/archive.zip'
 
 describe('helper: unzip', () => {
   it('should return "true" if archive has been extracted', async () => {
-    nock(baseUrl).post(file, { method: 'unzip' }).reply(204, {})
+    nock(baseUrl)
+      .post(file, { method: 'UNZIP' })
+      .reply(204, {})
     const result = await unzip(file, mockAxios)
     expect(result).toEqual(true)
   })
 
   it('should return "false" if archive has not been extracted', async () => {
-    nock(baseUrl).post(file, { method: 'unzip' }).reply(200, {})
+    nock(baseUrl)
+      .post(file, { method: 'UNZIP' })
+      .reply(200, {})
     const result = await unzip(file, mockAxios)
     expect(result).toEqual(false)
   })
 
   it('should throw an exception if server error occurs', async () => {
-    nock(baseUrl).post(file, { method: 'unzip' }).reply(500, {})
+    nock(baseUrl)
+      .post(file, { method: 'UNZIP' })
+      .reply(500, {})
     const check = unzip(file, mockAxios)
     await expect(check).rejects.toThrow()
   })
